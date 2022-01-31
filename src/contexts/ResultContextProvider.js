90122b0aeb3c44c6f0f9337c64fa076c31ffc675
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-31 22:08:36 +0700

    added: result context

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const ResultContext = createContext()
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-proxy-location': 'ID',
+        'x-rapidapi-key': '22faaa629emsh5679360207d5987p18cb0ejsn14038d317e16',
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
