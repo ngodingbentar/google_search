6a9d1982a9b8fb7d10419069460eae28b5ef26c7
ngodiingbentar <dewaqintoro@gmail.com>
2022-01-31 21:23:59 +0700

    initial switch theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ darkTheme, setDarkTheme, }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
@@ -10,7 +10,9 @@ const Navbar = () => {
           Goggle
         </p>
       </Link>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
     </div>
+    {/* <Search /> */}
   </div>
   )
 };
