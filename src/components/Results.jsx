4f160309d2aa6888e3d46a5b64aed9c7badbe67d
ngodiingbentar <dewaqintoro@gmail.com>
2022-02-05 03:47:17 +0700

    implement video results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -66,12 +66,12 @@ function Results() {
       );
     case '/videos':
       return (
-        <div className="flex flex-wrap ">videos
-          {/* {results?.results?.map((video, index) => (
+        <div className="flex flex-wrap ">
+          {results?.map((video, index) => (
             <div key={index} className="p-2">
               <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
             </div>
-          ))} */}
+          ))}
         </div>
       );
     default:
